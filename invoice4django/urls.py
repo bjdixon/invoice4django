9cb5ea3c9b497e21c05d30e395b3357203dc6666
bjdixon <bjdixon@gmail.com>
2014-02-19 15:20:46 -0500

    First unit test and url mapping... dummy view

--- invoice4django/urls.py
+++ invoice4django/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'invoice4django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	#url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'invoices.views.home_page', name='home'),
 )
--- invoices/tests/test_forms.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-# Probably import models and forms as well.
-
-
-class SomeFormTest(TestCase):
-
-	def test_form_does_something(self):
-		pass
