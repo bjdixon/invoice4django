d9df433b6a6ef30a634e0a100ddc1691a94bf65d
Brian J Dixon <bjdixon@gmail.com>
2014-03-01 18:57:27 -0500

    FT for seperation of invoices and user sessions

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -50,6 +50,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# After hitting enter he notices the page updates showing the entered values
 		# and new inputs for another line item
+		jolby_invoice_url = self.browser.current_url
+		self.assertRegex(jolby_invoice_url, '/invoices/.+')
 		self.check_for_row_in_invoice_table('Line 1: Item #1')
 
 		# He sees that he can add another line item and does so
@@ -67,8 +69,39 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_invoice_table('Line 2: Item #2')
 
 		
+		# A new user, Francis visits the site
 
+		## new browser session to make sure no information 
+		## from Jolby is coming through cookies etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
 
+		# Francis visits the home page. There's no sign of Jolby's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Line 1: Item #1', page_text)
+		self.assertNotIn('Line 2: Item #2', page_text)
+
+		# Francis starts a new list by entering a new item
+		line_item_input = self.browser.find_element_by_id('new_line_item_input')
+		line_item_input.send_keys('Francis Item #1')
+		line_item_description_input = self.find_element_by_id('new_line_item_description_input')
+		line_item_description_input.send_keys('Description for Francis Item #2')
+		line_item_quantity_input = self.find_element_by_id('new_line_item_quantity')
+		line_item_quantity_input.send_keys('1')
+
+		line_item_quantity_input.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_invoice_url = self.browser.current_url
+		self.assertRegex(francis_invoice_url, '/invoices/.+')
+		self.assertNotEqual(francis_invoice_url, jolby_invoice_url)
+
+		# again, no trace of Jolby's invoice
+		page_text = self.browser.find_element_by_tag_name('body')
+		self.assertNotIn('Line 1: Item #1', page_text)
+		self.assertIn('Line 1: Francis Item #1', page_text)
+		
 		# he notices that after each line item is added the net, tax and total 
 		# payable amounts increase by the correct amount
 
@@ -78,8 +111,3 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# he notices that he can enter the customer's email address and send the 
 		# invoice directly to them.
-
-
-		self.fail('finish the test')
-
-
